9b7974d964d72b91dd3061cb62a07e32cf9e52b9
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-02-08 19:43:42 +0500

    create project component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AboutComponent from './components/aboutComponent';
+import ProjectComponent from './components/projectComponent';
 
 function App() {
   return (
     <div className="App">
       <AboutComponent/>
+      <ProjectComponent/>
     </div>
   );
 }
---
+++ src/components/projectComponent/index.jsx
@@ -0,0 +1,12 @@
+import "./style.css"
+
+function ProjectComponent(props) {
+    return(
+        <div className="projectComp">
+            <div className="container">
+                <div className="projectsBox"></div>
+            </div>
+        </div>
+    )
+}
+export default ProjectComponent
