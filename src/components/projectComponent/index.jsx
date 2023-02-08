42d8981100e7c2b4ef197380b37e1c973c77de00
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-02-08 21:17:23 +0500

    editing project

--- src/components/projectComponent/index.jsx
+++ src/components/projectComponent/index.jsx
@@ -1,10 +1,56 @@
+import axios from "axios"
+import { useEffect } from "react";
+import { API_URL } from "../../utils/api"
 import "./style.css"
+import img from "../../windows.jpg"
 
 function ProjectComponent(props) {
+    // useEffect(()=> {
+
+    // },[])
     return(
         <div className="projectComp">
             <div className="container">
-                <div className="projectsBox"></div>
+                <h1>Bajarilgan ishlar</h1>
+                <ul>
+                    <li>
+                        <button value="1"></button>
+                        <img src={img} alt="img" />
+                        <h4>Another title</h4>
+                    </li>
+                    <li>
+                        <button value="1"></button>
+                        <img src={img} alt="img" />
+                        <h4>Another title</h4>
+                    </li>
+                    <li>
+                        <button value="1"></button>
+                        <img src={img} alt="img" />
+                        <h4>Another title</h4>
+                    </li>
+                    <li>
+                        <button value="1"></button>
+                        <img src={img} alt="img" />
+                        <h4>Another title</h4>
+                    </li>
+                    <li>
+                        <button value="1"></button>
+                        <img src={img} alt="img" />
+                        <h4>Another title</h4>
+                    </li>
+                    <li>
+                        <button value="1"></button>
+                        <img src={img} alt="img" />
+                        <h4>Another title</h4>
+                    </li>
+                    {/* {data.map(e => {
+                        <li key={e.id}>
+                            <button value={e.id}></button>
+                            <img src={e.img} alt="img" />
+                            <h4>{e.title}</h4>
+                        </li>
+                    })} */}
+                </ul>
             </div>
         </div>
     )
